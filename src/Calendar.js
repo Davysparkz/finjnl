e2d0442be4740a4af2bd11247c3914443f089cc2
Davysparkz <dabakura@gmail.com>
2022-12-08 01:16:55 +0100

    started embellishing the Calendar component with Tailwindcss

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,9 +1,25 @@
 import './Calendar.css';
+import dayjs from 'dayjs'
+import {useState} from 'react'
 
-function CalendarHeader({year, month}) {
+function DayHeader({dayText}) {
+    // TODO: `inline-block align-* not working`
+
     return (
-        <div className="CalendarHeader">
-            <p>{month}, {year}</p>
+        <div className="inline-block align-middle w-10 h-10 text-center">{dayText}</div>
+    )
+}
+
+function CalendarHeader() {
+    return (
+        <div className="CalendarHeader flex">
+            <DayHeader dayText={'M'}/>
+            <DayHeader dayText={'T'}/>
+            <DayHeader dayText={'W'}/>
+            <DayHeader dayText={'T'}/>
+            <DayHeader dayText={'F'}/>
+            <DayHeader dayText={'S'}/>
+            <DayHeader dayText={'S'}/>
         </div>
     )
 }
@@ -11,41 +27,74 @@ function CalendarHeader({year, month}) {
 function CalendarBody() {
     return (
         <div className="CalendarBody">
-
+            <CalendarMonth numOfDays={31} startWeekDay={2} endWeekDay={3}></CalendarMonth>
         </div>
     )
 }
 
-function CalendarYear({currentYear}) {
+function CalendarYear() {
     return (
         <div className="CalendarYear">
-
+           {dayjs().format('YYYY')}
         </div>
     )
 }
 
-function CalendarMonth() {
-    return (
-        <div className="CalendarMonth">
+function CalendarDay({day}) {
 
+    return (
+        <div className="CalendarDay bg-red-200 w-10 h-10 text-center font-bold">
+            {day}
         </div>
     )
 }
 
-function CalendarDay() {
-    return (
-        <div className="CalendarDay">
+function CalendarWeek({startDay, endDay}) {
 
+    const getDaysPerRow = () => {
+        const daysPerRow = [];
+
+        for (let i = startDay; i <= endDay; i++) {
+            daysPerRow.push(<CalendarDay day={i} key={i}/>)
+        }
+        return daysPerRow
+    }
+
+    return (
+        <div className="CalendarWeek flex bg-blue-400 justify-center divide-x-2 divide-blue-400">
+            {getDaysPerRow()}
+        </div>
+    )
+}
+
+function CalendarMonth({numOfDays, startWeekDay, endWeekDay}) {
+    const getDaysPerCol= () => {
+        const daysPerCol = [];
+
+        let i = 0
+        let startDay = i + 1
+        let endDay = startDay + 6
+
+        for (i; i <= numOfDays;) {
+            daysPerCol.push(<CalendarWeek startDay={startDay} endDay={endDay} key={i}/>)
+            i = endDay
+            startDay = i + 1
+            endDay = startDay + 6
+        }
+        return daysPerCol
+    }
+    return (
+        <div className="CalendarMonth flex flex-col divide-y-2 divide-blue-400">
+            {getDaysPerCol()}
         </div>
     )
 }
 
 function Calendar({year, month}) {
     return (
-      <div className="Calendar">
-        <CalendarHeader year={year} month={month}></CalendarHeader>
-        <CalendarYear currentYear={year}>
-        </CalendarYear>
+      <div className="Calendar ">
+        <CalendarHeader></CalendarHeader>
+        <CalendarBody></CalendarBody>
       </div>
     );
   }
